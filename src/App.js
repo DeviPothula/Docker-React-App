3744c8b351130fdbef47c324848adda5632b7eca
DeviPothula <devikapothula597@gmail.com>
2024-11-12 15:36:09 +0530

    removed sonarbugs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 function App() {
   // Unused variable
-  const unusedVariable = "I am not used";
-
-  // Dummy function with duplicate code
-  function duplicateFunction() {
-    console.log("This is duplicate code");
-  }
-
-  function anotherDuplicateFunction() {
-    console.log("This is duplicate code");
-  }
 
   // Console.log statements, often flagged as code smells
   console.log("Testing SonarQube bugs");
@@ -18,12 +8,8 @@ function App() {
   // Commented-out code
   // const commentedOutVariable = "This should not be here";
 
-  if (true) {
-    // Empty block
-  }
-
   return (
-    <div className="App" >
+    <div className="App">
       <h2>This is my first docker image</h2>
       <p>Hi devi!!</p>
     </div>
