884e7cc08c7cff62493fee954bf78a2d8f8a48d6
DeviPothula <devikapothula597@gmail.com>
2024-11-12 15:17:51 +0530

    Added un used code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 function App() {
-  //...
-  if (true) {
-    
+  // Unused variable
+  const unusedVariable = "I am not used";
+
+  // Dummy function with duplicate code
+  function duplicateFunction() {
+    console.log("This is duplicate code");
   }
+
+  function anotherDuplicateFunction() {
+    console.log("This is duplicate code");
+  }
+
+  // Console.log statements, often flagged as code smells
+  console.log("Testing SonarQube bugs");
+  console.log("Another log statement");
+
+  // Commented-out code
+  // const commentedOutVariable = "This should not be here";
+
+  if (true) {
+    // Empty block
+  }
+
   return (
     <div className="App" >
       <h2>This is my first docker image</h2>
